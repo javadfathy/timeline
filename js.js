2ba50f3910a1ad4fdd636173b1270843a1ee39ac
javadfathi <62674928+javadfathy@users.noreply.github.com>
2022-05-06 15:51:15 +0430

    Update js.js

--- js.js
+++ js.js
@@ -1,56 +1,67 @@
-$(document).ready(function(){
+    $(document).ready(function(){
 
 
-	var timelineSpan = $(".line-timeline").find("span");
-	var contentTimeline = $(".content-timeline").find("p");
+        var timelineSpan = $(".circle-timeline").find("div");
+        var contentTimeline = $(".content-timeline").find("p");
 
-	timelineSpan.click(function(){
-		var indexSpan = $(this).index();
+        timelineSpan.click(function(){
+            var indexSpan = $(this).index();
+
+            contentTimeline.removeClass("active-content-timeline");
+            setTimeout(function(){
+                contentTimeline.eq(indexSpan).addClass("active-content-timeline");
+            },600)
+
+
+            timelineSpan.removeClass("active-timeline");
+            $(this).addClass("active-timeline");
+
+            console.log($(this).index());
+        });
 		
-		contentTimeline.removeClass("active-content-timeline");
-		setTimeout(function(){
-			contentTimeline.eq(indexSpan).addClass("active-content-timeline");
-		},600)
 		
+		contentTimeline.click(function(){
+            var indexSpan = $(this).index();
 
-		timelineSpan.removeClass("active-timeline");
-		$(this).addClass("active-timeline");
-		
-		console.log($(this).index());
-	});
-
-	var timerTimeline = setInterval(autoTimeline,3000);
-
-
-	$('.timeline').hover(function(ev){
-	    clearInterval(timerTimeline);
-	}, function(ev){
-	    timerTimeline = setInterval(autoTimeline,3000);
-	});
-
-	function autoTimeline(){
-		if($("#timeline6").hasClass("active-timeline")){
-				timelineSpan.removeClass("active-timeline");
-	    		contentTimeline.removeClass("active-content-timeline");
-				timelineSpan.eq(0).addClass("active-timeline");
-				contentTimeline.eq(0).addClass("active-content-timeline");
-			
-		}else{
-	    	var indexSpanActive = $(".line-timeline").find(".active-timeline").index();
-	    	var indexContentTimelineActive = $(".content-timeline").find(".active-content-timeline").index();
-
-	    	timelineSpan.removeClass("active-timeline");
-	    	contentTimeline.removeClass("active-content-timeline");
+            timelineSpan.removeClass("active-timeline");
+            setTimeout(function(){
+                timelineSpan.eq(indexSpan).addClass("active-timeline");
+            },400)
 
 
-			timelineSpan.eq(indexSpanActive + 1).addClass("active-timeline");
-			contentTimeline.eq(indexContentTimelineActive + 1).addClass("active-content-timeline");
-		}
-	}
+            contentTimeline.removeClass("active-content-timeline");
+            $(this).addClass("active-content-timeline");
+
+            console.log($(this).index());
+        });
+
+        var timerTimeline = setInterval(autoTimeline,5000);
 
 
-	
+        $('.timelineAboutus').hover(function(ev){
+            clearInterval(timerTimeline);
+        }, function(ev){
+            timerTimeline = setInterval(autoTimeline,5000);
+        });
+
+        function autoTimeline(){
+            if($(".timeline6").hasClass("active-timeline")){
+                timelineSpan.removeClass("active-timeline");
+                contentTimeline.removeClass("active-content-timeline");
+                timelineSpan.eq(0).addClass("active-timeline");
+                contentTimeline.eq(0).addClass("active-content-timeline");
+
+            }else{
+                var indexSpanActive = $(".line-timeline").find(".active-timeline").index();
+                var indexContentTimelineActive = $(".content-timeline").find(".active-content-timeline").index();
+
+                timelineSpan.removeClass("active-timeline");
+                contentTimeline.removeClass("active-content-timeline");
 
 
+                timelineSpan.eq(indexSpanActive + 1).addClass("active-timeline");
+                contentTimeline.eq(indexContentTimelineActive + 1).addClass("active-content-timeline");
+            }
+        }
 
-})
+    })
